e41a40a304b0368027d3f4a2ecc0d3d6dc6d7a24
vitaxa <vitaxa93gamebox@gmail.com>
2023-10-11 17:30:53 +0300

    WIP

--- src/main/kotlin/dev/crashteam/uzumanalytics/config/AwsSteamConfig.kt
+++ src/main/kotlin/dev/crashteam/uzumanalytics/config/AwsSteamConfig.kt
@@ -3,15 +3,19 @@ package dev.crashteam.uzumanalytics.config
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.dynamodbv2.model.BillingMode
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration.*
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.SimpleRecordsFetcherFactory
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker
+import com.amazonaws.services.kinesis.metrics.impl.NullMetricsFactory
 import dev.crashteam.uzumanalytics.config.properties.AwsStreamProperties
 import dev.crashteam.uzumanalytics.stream.listener.aws.UzumEventStreamProcessorFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.time.Duration
 
 @Configuration
 class AwsSteamConfig(
@@ -28,15 +32,15 @@ class AwsSteamConfig(
         val consumerConfig = KinesisClientLibConfiguration(
             appName,
             awsStreamProperties.uzumStream.name,
-            awsStreamProperties.endpoint,
-            "",
-            DEFAULT_INITIAL_POSITION_IN_STREAM,
+            "yds.serverless.yandexcloud.net/ru-central1/b1gtojcphtuae1n9siie/etnvck8jo3kuh0vilc57",
+            "docapi.serverless.yandexcloud.net/ru-central1/b1gtojcphtuae1n9siie/etnvck8jo3kuh0vilc57",
+            InitialPositionInStream.LATEST,
             AWSStaticCredentialsProvider(awsCredentials),
             AWSStaticCredentialsProvider(awsCredentials),
             AWSStaticCredentialsProvider(awsCredentials),
             DEFAULT_FAILOVER_TIME_MILLIS,
             "$appName-consumer",
-            DEFAULT_MAX_RECORDS,
+            100,
             DEFAULT_IDLETIME_BETWEEN_READS_MILLIS,
             DEFAULT_DONT_CALL_PROCESS_RECORDS_FOR_EMPTY_RECORD_LIST,
             DEFAULT_PARENT_SHARD_POLL_INTERVAL_MILLIS,
@@ -51,16 +55,17 @@ class AwsSteamConfig(
             DEFAULT_VALIDATE_SEQUENCE_NUMBER_BEFORE_CHECKPOINTING,
             awsStreamProperties.region,
             DEFAULT_SHUTDOWN_GRACE_MILLIS,
-            DEFAULT_DDB_BILLING_MODE,
-            null,
-            0,
-            0,
-            0
+            BillingMode.PAY_PER_REQUEST,
+            SimpleRecordsFetcherFactory(),
+            Duration.ofMinutes(1).toMillis(),
+            Duration.ofMinutes(5).toMillis(),
+            Duration.ofMinutes(30).toMillis(),
         )
 
         return Worker.Builder()
             .recordProcessorFactory(uzumEventStreamProcessor)
             .config(consumerConfig)
+            .metricsFactory(NullMetricsFactory())
             .build()
     }
 }
