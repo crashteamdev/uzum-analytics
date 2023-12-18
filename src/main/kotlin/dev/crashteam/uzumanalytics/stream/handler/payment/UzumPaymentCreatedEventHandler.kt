1d90004b8ffd6ac06e71b77b261c11f32049c37d
vitaxa <vitaxa93gamebox@gmail.com>
2023-12-18 23:49:51 +0300

    fix payment handler

--- src/main/kotlin/dev/crashteam/uzumanalytics/stream/handler/payment/UzumPaymentCreatedEventHandler.kt
+++ src/main/kotlin/dev/crashteam/uzumanalytics/stream/handler/payment/UzumPaymentCreatedEventHandler.kt
@@ -1,14 +1,16 @@
 package dev.crashteam.uzumanalytics.stream.handler.payment
 
+import dev.crashteam.payment.PaymentCreated
 import dev.crashteam.payment.PaymentEvent
+import dev.crashteam.payment.PaymentStatus
 import dev.crashteam.payment.UzumAnalyticsContext
 import dev.crashteam.uzumanalytics.domain.mongo.PaymentDocument
 import dev.crashteam.uzumanalytics.extension.toLocalDateTime
 import dev.crashteam.uzumanalytics.repository.mongo.PaymentRepository
+import dev.crashteam.uzumanalytics.service.PaymentService
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import org.springframework.core.convert.ConversionService
 import org.springframework.stereotype.Component
 
 private val log = KotlinLogging.logger {}
@@ -16,7 +18,7 @@ private val log = KotlinLogging.logger {}
 @Component
 class UzumPaymentCreatedEventHandler(
     private val paymentRepository: PaymentRepository,
-    private val conversionService: ConversionService,
+    private val paymentService: PaymentService,
 ) : PaymentEventHandler {
 
     override fun handle(events: List<PaymentEvent>) {
@@ -27,21 +29,17 @@ class UzumPaymentCreatedEventHandler(
 
                 if (paymentDocument != null) continue
 
-                val paymentStatus = conversionService.convert(paymentCreated.status, String::class.java)!!
-                val newPaymentDocument = PaymentDocument(
-                    paymentId = paymentCreated.paymentId,
-                    userId = paymentCreated.userId,
-                    createdAt = paymentCreated.createdAt.toLocalDateTime(),
-                    status = paymentStatus,
-                    paid = false,
-                    amount = paymentCreated.amount.value.toBigDecimal().movePointLeft(2),
-                    multiply = paymentCreated.userPaidService.paidService.context.multiply.toShort(),
-                    subscriptionType = mapProtoSubscriptionPlan(
-                        paymentCreated.userPaidService.paidService.context.uzumAnalyticsContext.plan
+                if (paymentCreated.status == PaymentStatus.PAYMENT_STATUS_SUCCESS) {
+                    log.info { "Create payment with final state. paymentId=${paymentCreated.paymentId}; userId=${paymentCreated.userId}" }
+                    createPayment(paymentCreated)
+                    paymentService.callbackPayment(
+                        paymentId = paymentCreated.paymentId,
+                        userId = paymentCreated.userId,
                     )
-                )
-                log.info { "Save new payment: $newPaymentDocument" }
-                paymentRepository.save(newPaymentDocument).awaitSingleOrNull()
+                } else {
+                    log.info { "Create payment. paymentId=${paymentCreated.paymentId}; userId=${paymentCreated.userId}" }
+                    createPayment(paymentCreated)
+                }
             }
         }
     }
@@ -51,6 +49,32 @@ class UzumPaymentCreatedEventHandler(
                 event.payload.paymentChange.hasPaymentCreated() &&
                 event.payload.paymentChange.paymentCreated.hasUserPaidService() &&
                 event.payload.paymentChange.paymentCreated.userPaidService.paidService.context.hasUzumAnalyticsContext()
+    }
+
+    private suspend fun createPayment(paymentCreated: PaymentCreated) {
+        val newPaymentDocument = PaymentDocument(
+            paymentId = paymentCreated.paymentId,
+            userId = paymentCreated.userId,
+            createdAt = paymentCreated.createdAt.toLocalDateTime(),
+            status = mapPaymentStatus(paymentCreated.status),
+            paid = false,
+            amount = paymentCreated.amount.value.toBigDecimal().movePointLeft(2),
+            multiply = paymentCreated.userPaidService.paidService.context.multiply.toShort(),
+            subscriptionType = mapProtoSubscriptionPlan(
+                paymentCreated.userPaidService.paidService.context.uzumAnalyticsContext.plan
+            )
+        )
+        paymentRepository.save(newPaymentDocument).awaitSingleOrNull()
+    }
+
+    private fun mapPaymentStatus(paymentStatus: PaymentStatus): String {
+        return when (paymentStatus) {
+            PaymentStatus.PAYMENT_STATUS_PENDING -> "pending"
+            PaymentStatus.PAYMENT_STATUS_SUCCESS -> "success"
+            PaymentStatus.PAYMENT_STATUS_CANCELED -> "canceled"
+            PaymentStatus.PAYMENT_STATUS_FAILED -> "failed"
+            PaymentStatus.PAYMENT_STATUS_UNKNOWN, PaymentStatus.UNRECOGNIZED -> "unknown"
+        }
     }
 
     private fun mapProtoSubscriptionPlan(uzumPlan: UzumAnalyticsContext.UzumAnalyticsPlan): Int {
